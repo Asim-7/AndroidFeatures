284cfc18c852d47de4fbe289357f144149d00139
Asim <asimpsy11@gmail.com>
2023-11-14 10:19:41 +0100

    Correct app format added

--- app/src/main/java/com/asim/androidfeatures/MainActivity.kt
+++ app/src/main/java/com/asim/androidfeatures/MainActivity.kt
@@ -2,10 +2,30 @@ package com.asim.androidfeatures
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.asim.androidfeatures.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
+        setContentView(mainBinding.root)
+
+        with(mainBinding) {
+            screenshotButton.setOnClickListener {
+                // TODO
+            }
+            openFileButton.setOnClickListener {
+                // TODO
+            }
+            saveFileButton.setOnClickListener {
+                // TODO
+            }
+        }
     }
 }
